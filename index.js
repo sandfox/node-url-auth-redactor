0cdc62193bff3f15ecb4f1e1c82fb61b52f3ed00
James Butler <james.butler@sandfox.co.uk>
2016-10-10 15:39:34 +0100

    fix cases where url contains no auth

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ var url = require('url')
 
 module.exports = function redact(urlString){
   var parts = url.parse(urlString)
-  var auth = parts.auth.split(':')
-  if(auth[1] && auth[1].length > 0){
-    auth[1] = '****'
-  }
-  parts.auth = auth.join(':')
+  var auth = null
 
+  if(parts.auth) {
+    auth = parts.auth.split(':')
+  }
+
+  if(auth && auth[1] && auth[1].length > 0){
+    auth[1] = '****'
+    parts.auth = auth.join(':')
+  }
+  
   return url.format(parts)
 }
--- test.js
+++ test.js
@@ -6,3 +6,8 @@ var expectedRedactedUrl = 'https://james:****@localhost.com:4345/yeah'
 var redactedUrl = redactor(url)
 
 assert.equal(redactedUrl, expectedRedactedUrl)
+
+
+var noAuthUrl = 'https://localhost.com:4345/yeah'
+var unRedactedUrl = redactor(noAuthUrl)
+assert.equal(unRedactedUrl, noAuthUrl)
