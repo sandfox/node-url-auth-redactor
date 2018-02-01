1038b713893a2fb8fcb71d981a76fe4b5cbe80b2
Dominic Smith <dom.smithy@gmail.com>
2018-02-01 10:52:49 +1000

    Work for urls without auth component

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var url = require('url')
 
 module.exports = function redact(urlString){
   var parts = url.parse(urlString)
-  var auth = parts.auth.split(':')
-  if(auth[1] && auth[1].length > 0){
-    auth[1] = '****'
+
+  if (parts.auth) {
+    var auth = parts.auth.split(':')
+    if(auth[1] && auth[1].length > 0){
+      auth[1] = '****'
+    }
+    parts.auth = auth.join(':')
   }
-  parts.auth = auth.join(':')
 
   return url.format(parts)
 }
